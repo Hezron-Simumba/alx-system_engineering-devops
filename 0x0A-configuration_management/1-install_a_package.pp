f9e69e1a42b00fc7db44a56a6486738e081a1a30
Hezron <hezron@localhost.localdomain>
2024-06-09 17:30:31 +0200

    code update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,9 @@
 #!/usr/bin/pup
 # Install an especific version of flask (2.1.0)
-package {'flask':
+exec { 'install python packages':
+  command => 'pip3 install flask',
+  path    => ['/usr/bin/'],
+  unless  => '/usr/bin/test -f /usr/local/lib/python3.4/dist-packages/flask/app.py',
   ensure   => '2.1.0',
   provider => 'pip3'
 }
